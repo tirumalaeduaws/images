601bf7f938d1562ab7994e3841e1f7eb160d7b42
tirumalaeduaws <tirumalaeduaws@gmail.com>
2021-04-24 09:35:09 +0530

    kl

--- application/logs/log-2020-11-01.php
+++
@@ -1,4 +0,0 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
-
-ERROR - 2020-11-01 20:35:28 --> 404 Page Not Found: Contacthtml/index
-ERROR - 2020-11-01 20:35:28 --> 404 Page Not Found: Contacthtml/index
--- application/logs/log-2021-04-23.php
+++
@@ -1,4 +0,0 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
-
-ERROR - 2021-04-23 12:58:57 --> Severity: error --> Exception: Too few arguments to function Admin_model::get_expadmission(), 2 passed in C:\xampp\htdocs\tirumala\application\controllers\Admin.php on line 413 and exactly 4 expected C:\xampp\htdocs\tirumala\application\models\Admin_Model.php 151
-ERROR - 2021-04-23 13:00:40 --> Severity: error --> Exception: Too few arguments to function Admin_model::get_expadmission(), 2 passed in C:\xampp\htdocs\tirumala\application\controllers\Admin.php on line 413 and exactly 4 expected C:\xampp\htdocs\tirumala\application\models\Admin_Model.php 151
